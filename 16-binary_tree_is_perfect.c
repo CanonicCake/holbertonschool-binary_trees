8ae4756bf005aa46ea49c451ea551ae1e1c0c0ce
CanonicCake <n.a.ellis99@gmail.com>
2023-08-23 19:31:56 -0700

    betty style fixed

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -3,7 +3,7 @@
 #include "11-binary_tree_size.c"
 
 /**
- * binar_tree_is_perfect - checks if binary tree is perfect
+ * binary_tree_is_perfect - checks if binary tree is perfect
  * @tree: tree pointer
  *
  * Return: tree on success, 0 if tree is NULL
@@ -27,7 +27,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 
 	left_size = binary_tree_size(tree->left);
-	right_size = binary_tree_size(tree-> right);
+	right_size = binary_tree_size(tree->right);
 
 	if (left_size != right_size)
 		return (0);
