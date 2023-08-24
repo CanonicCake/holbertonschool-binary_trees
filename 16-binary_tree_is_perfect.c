40cdc8457de83b1ee6a0e391899c1068d003e6ae
CanonicCake <n.a.ellis99@gmail.com>
2023-08-23 19:30:00 -0700

    #include in main fix, added size

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,4 +1,6 @@
 #include "binary_trees.h"
+#include "9-binary_tree_height.c"
+#include "11-binary_tree_size.c"
 
 /**
  * binar_tree_is_perfect - checks if binary tree is perfect
@@ -9,5 +11,29 @@
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	
+	int left, right;
+	size_t left_size, right_size;
+
+	if (!tree)
+		return (0);
+
+	if (!tree->left && !tree->right)
+		return (1);
+
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+
+	if (left != right)
+		return (0);
+
+	left_size = binary_tree_size(tree->left);
+	right_size = binary_tree_size(tree-> right);
+
+	if (left_size != right_size)
+		return (0);
+
+	if (binary_tree_is_perfect(tree->left) &&
+			binary_tree_is_perfect(tree->right))
+		return (1);
+	return (0);
 }
